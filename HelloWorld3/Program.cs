958a74a428e7994fa3908e28606dd8e560a52389
fRenzy <fRenzyA05@gmail.com>
2022-09-13 09:13:15 -0300

    Initial commit

---
+++ HelloWorld3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD 33333333333");
+            Console.ReadKey(true);
+        }
+    }
+}
